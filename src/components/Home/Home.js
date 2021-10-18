2d3c9f52521a447711e767a778f913c4b5d95726
= <=>
2021-10-18 12:42:34 +0600

    home page banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Header from "../Header/Header";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is Home</h1>
+    <div className="home-banner d-flex justify-content-evenly align-items-center">
+      <div className="w-50 p-5">
+        <h1 className="fw-bold">
+          Leading you to a <span className="text-gradient">Healthy</span> life
+        </h1>
+        <p className="text-white">
+          In today’s modern world, many of our systems and customs seem to be
+          organized in a way that separates the different facets of health- for
+          example, a nutritionist might recommend ways to eat healthfully, and a
+          therapist might recommend ways to cope with stress, but often, there
+          is little to no crossover guidance, or framework for fitting the
+          pieces together as a whole.
+        </p>
+      </div>
+      <div></div>
     </div>
   );
 };
