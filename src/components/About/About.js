f18cc6ee9ad56e269f35ac4183108495e44a1ce4
= <=>
2021-10-19 13:37:29 +0600

    about page added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,122 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>This is About Page</h1>
+    <div className="about">
+      <div className="about-container">
+        <div className="half">
+          <h1>About Us</h1>
+          <p>
+            <b>Medify</b> is a dream that turned into a tech based startup, a
+            business with a great purpose to create positive impacts in peoples'
+            lives. It is the first online doctor appointment service platform in
+            Bangladesh, providing real time doctor information and appointments
+            through a fully integrated system. <b>Medify</b> is missioned to
+            bring convenience in the healthcare service delivery for the general
+            people in Bangladesh.
+          </p>
+          <h4>Background</h4>
+          <hr />
+          <p>
+            The business was founded with a strong will to solve a real problem
+            that impacts many in the country. The thought was kindled by the
+            observation that people living in places beyond the major cities
+            face tremendous troubles in finding a right doctor at the right time
+            and subsequently suffer in securing treatment when they need it.
+            Bangladesh is making significant progresses in many areas.
+            Information technology, without any doubt has been instrumental in
+            the transformation of people's way of doing things. We have seen
+            bus, train or launch tickets being bought online. Online freelancing
+            bringing new skills, confidence and earnings to thousands of youths.
+            Noticeable boost in the e-commerce. Use of mobile money making lives
+            better. Ironically, despite of being one of the most essential
+            services, healthcare has not been focused with similar initiatives
+            yet. There are still roadblocks disrupting smooth healthcare
+            delivery even today. And, information technology has rarely been
+            called up to effectively contribute in the sector.
+          </p>
+          <h4>Evolution into a Service Platform</h4>
+          <hr />
+          <p>
+            The idea of <b>Medify</b> evolved into a service platform in early
+            2014. It was not what it is today in the initial days. It started as
+            a doctor's directory along with a feature for appointment requests.
+            After realizing that only accomplishing appointment bookings will
+            not help much, the whole vision was revisited. We needed to offer
+            more. We needed to build and maintain relationships with the
+            patients and doctors to ensure smoother connection for the service.
+            We needed to add new features and extend the whole platform to
+            accommodate many things that people will never see working as the
+            back office. <b>Medify</b> understands the individuality of each
+            patient and their problem. The platform is capable of maintaining
+            records of queries, appointments, visits and visit outcomes. With
+            the aid of a huge knowledge base built and maintained within the
+            platform,
+            <b>Medify</b> can follow up and take extended care of their users
+            for their unique problems. Further, for ensuring real time
+            availability of the doctors at their chambers, a dedicated team
+            constantly engages with the hospitals, clinics and chambers'
+            management.
+          </p>
+          <h4>Changing Mindsets</h4>
+          <hr />
+          <p>
+            <b>Medify</b> aims to drive two very important behavioral changes
+            that can make approaching treatment faster and better. In general, a
+            common person in our country is quite unwilling to see a proper
+            physician for his or her illness, until it gets too bad. People try
+            out different suggestions from friends around them or conduct a self
+            treatment based on other's experiences. This is mainly due to
+            serious lack of awareness about the signs, symptoms, consequences of
+            diseases and lack of proper guidance. Secondly, when people want to
+            approach treatment, they commonly get into the trouble of finding a
+            right doctor. In most cases, they head to Dhaka or other big cities
+            and ask their relatives or friends for help. Unfortunately, those
+            relatives or friends typically know only a few doctor names. Those
+            are generally the busiest doctors who are well known at the national
+            level. Certainly, taking an appointment of those doctors are quite a
+            hassle. One may need to wait even two-three months to consult the
+            doctor. This is again mainly because, there is not enough awareness
+            about the availability of many qualified doctors across the country
+            who could give attention and treat the patient earlier than the time
+            he/she will wait for. These result in unnecessary delays aggravating
+            the condition of the disease. <b>Medify</b> through its social
+            network platform and many other means is continuously engaging to
+            build the awareness among people to help these improve.
+          </p>
+          <h4>Expansion with Impact</h4>
+          <hr />
+          <p>
+            If we are to truly improve this with the right impact, we must focus
+            on the people at the rural or in small cities. Providing and
+            extending this service to the rural people of Bangladesh requires a
+            lot of resources and activities. And, it isn't easy with the limited
+            resources.
+          </p>
+          <h4>Present Operation</h4>
+          <hr />
+          <p>
+            Since the official launching on 31st October 2015, <b>Medify</b> is
+            operational with full capacity in regard to providing appointments
+            from doctors around the country. The service hosts information and
+            appointment facilities for more than 9615 doctors from 556
+            consultation centers in 63 districts. We have served a few thousand
+            patients so far and have been able to bring smiles on their faces.
+          </p>
+          <h4>The Future</h4>
+          <hr />
+          <p>
+            At <b>Medify</b>, we are confident to make a difference in how
+            people of Bangladesh approach healthcare services. We have plans to
+            integrate many other related services with the doctors' appointment
+            in the platform in coming days. The investment from BD Venture has
+            given the initiative more speed. Now, it's all about execution of
+            the dreams to bring goodness out for everyone in the country.
+          </p>
+        </div>
+        <div className="half"></div>
+      </div>
     </div>
   );
 };
