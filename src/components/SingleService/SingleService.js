26eca3ae0a21c49866ce029e2cd8a11df3cf7e40
= <=>
2021-10-19 13:50:41 +0600

    working days added

--- src/components/SingleService/SingleService.js
+++ src/components/SingleService/SingleService.js
@@ -2,30 +2,36 @@ import React from "react";
 import "./SingleService.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faCalendarWeek,
   faDollarSign,
   faHospitalUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const SingleService = (props) => {
-  const { name, img, id, price } = props.service;
+  const { name, img, id, price, working } = props.service;
 
   //icons
   const service = <FontAwesomeIcon icon={faHospitalUser} />;
   const cost = <FontAwesomeIcon icon={faDollarSign} />;
+  const calender = <FontAwesomeIcon icon={faCalendarWeek} />;
   return (
     <div className="service-card shadow-lg">
       <div className="service-img">
         <img src={img} alt="service" />
       </div>
       <div className="service-info p-3">
-        <h3>
+        <h4>
           <span className="text-danger">{service} </span>
           {name}
-        </h3>
+        </h4>
         <p className="fw-bold">
           <span className="text-success">{cost} </span>
           {price}
+        </p>
+        <p>
+          <b>{calender} </b>
+          {working}
         </p>
         <Link to={`/service/${id}`}>
           <button className="btn btn-info fw-bold">View</button>
